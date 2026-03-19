fda455a931c16f03a6a2b52f58f9561fec5c9b14
krazzyaiguy <krazzyaiguy@gmail.com>
2026-03-20 01:16:51 +0530

    Update GmailSync.jsx

--- src/components/GmailSync.jsx
+++ src/components/GmailSync.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ls } from "../utils/storage";
 import { resolvePasswords, extractHintsFromEmail } from "../utils/passwords";
 import { pdfBytesToBase64Image, tryPasswordsOnPDF, callGroq, fetchStatementEmails, fetchEmailWithAttachments, downloadAttachment } from "../utils/pdfGroqGmail";
